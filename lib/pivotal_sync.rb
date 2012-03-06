76ddd93796a82540eb2b731e05953a99d9d89a66
Hemant Kumar <gethemant@gmail.com>
2012-03-06 16:14:09 +0530

    More debugging stuff for messaging

--- lib/pivotal_sync.rb
+++ lib/pivotal_sync.rb
@@ -4,13 +4,14 @@ class PivotalSync
     new(pivotal_project_id,redmine_project_id)
   end
 
-  attr_accessor :pivotal_project_id, :redmine_project_id
+  attr_accessor :pivotal_project_id, :redmine_project_handle
 
-  def initialize(pivotal_project_id,redmine_project_id)
+  def initialize(pivotal_project_id,redmine_project_handle)
     @pivotal_project_id = pivotal_project_id
-    @redmine_project_id = redmine_project_id
+    @redmine_project_handle = redmine_project_handle
 
     @project = PivotalTracker::Project.find(pivotal_project_id)
+    raise "Invalid pivotal project id #{pivotal_project_id}" unless @project
     populate_already_synced_issues
   end
 
@@ -23,8 +24,16 @@ class PivotalSync
     end
   end
 
+  def redmine_project
+    Project.find_by_id(redmine_project_handle) ||
+      Project.find_by_name(redmine_project_handle)
+  end
+
   def sync_all_open_issues
-    issues = Project.find(redmine_project_id).issues.open
+    project = redmine_project()
+    raise "Invalid redmine project #{redmine_project_handle}" unless project
+
+    issues = project.issues.open
     issues.each do |issue|
       print "."
       next if @synced_issues.include(issue.id)
--- lib/tasks/sync_ticket.rake
+++ lib/tasks/sync_ticket.rake
@@ -1,6 +1,6 @@
 desc "Sync all open tickets to pivotal"
 task :sync_tickets do
-  redmine_project_id = ENV['redmine_project_id']
+  redmine_project = ENV['redmine_project_id'] || ENV['redmine_project_name']
   pivotal_project_id = ENV['pivotal_project_id']
-  PivotalSync.sync(pivotal_project_id,redmine_project_id)
+  PivotalSync.sync(pivotal_project_id,redmine_project)
 end
